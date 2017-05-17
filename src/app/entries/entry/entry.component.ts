81ec7510a6464101c133e0e0b75c9338b9f1a97c
anontyro <alexwilkinson@gmail.com>
2017-05-17 22:49:17 +0800

    adding some of the basic functions for the app, creating a dummy example that sets up all the image and comments correctly

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -7,5 +7,16 @@ import {Component} from '@angular/core';
 })
 
 export class EntryComponent{
+    title: string = 'My Photo';
+    photo: string = 'http://placehold.it/800x500?text=AngularBasics';
+    description: string = 'A description of the photo';
 
+    comments: any[] = [
+        {name: "John", comment: "A pretty good comment"},
+        {name: "Sam", comment: "A pretty good comment"},
+        {name: "Alex", comment: "A pretty good comment"},
+        {name: "Shwe", comment: "A pretty good comment"},
+        {name: "Ben", comment: "A pretty good comment"}
+        
+    ]
 }
