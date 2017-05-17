361666636bbf0e9e3da8e3a2a0b866e72b0bcba0
anontyro <alexwilkinson@gmail.com>
2017-05-17 17:00:28 +0800

    added basic function to check that the application works

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: '<h2>Hello World</h2>',
+    styles: [
+        `
+        h2{
+            color: red;
+        }
+        `
+    ]
+})
+export class AppComponent{
+
+}
---
+++ wwwroot/app.c674ca0abf49e831fb1d.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],[function(t,e,c){"use strict";c(1)},function(t,e){t.exports="// removed by extract-text-webpack-plugin"}]);
+//# sourceMappingURL=app.c674ca0abf49e831fb1d.js.map
