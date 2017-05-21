9bc8d73c67b442953dc5269d0541759bc6a4eb27
anontyro <alexwilkinson@gmail.com>
2017-05-21 21:49:53 +0800

    added form validation everything working with a basic inbuilt inmemory database

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,4 +1,6 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import{NgForm} from '@angular/forms';
+import{EntryService} from '../shared/entry.service';
 
 @Component({
     selector: 'app-entry-comment-form',
@@ -8,10 +10,23 @@ import {Component, EventEmitter, Output} from '@angular/core';
 export class EntryCommentFormComponent{
     name: string = "";
     comment: string = "";
+    @Input() entryId: number;
     @Output() onCommentAdded = new EventEmitter<{name: string; comment: string;}>();
-    onSubmit(){
+    @ViewChild('commentForm') commentForm: NgForm;
+
+    constructor(private entryService: EntryService){
+
+    }
+
+    onSubmit(commentForm: NgForm){
+        if(this.commentForm.invalid) return;
         let comment = {name: this.name, comment: this.comment};
-        this.onCommentAdded.emit(comment);
+        this.entryService.addComment(this.entryId, comment)
+            .then(() =>{
+                this.onCommentAdded.emit(comment);
+                this.commentForm.resetForm();
+            });
+
     }
 
 }
